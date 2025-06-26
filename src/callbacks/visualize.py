ee6aff416697310d7d45db576ddec302cb0fa6b0
Farida Yusuf <f.yusuf@qmul.ac.uk>
2025-06-26 15:51:08 +0000

    Generate and save viz callback plots

--- src/callbacks/visualize.py
+++ src/callbacks/visualize.py
@@ -1,25 +1,46 @@
 from lightning.pytorch.callbacks import Callback
 import torch
-import viz 
+import viz
 
-# TODO: callback to save/collect plot at each validation step
-# (i.e. per epoch) then on training end, create and save/display GIF
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE, Isomap
+from umap.umap_ import UMAP
+
+from typing import Union, Any
+from pathlib import Path
+
+# TODO: dict of constants to choose cmap
+# based on label name
+
+TEST_SAVE_PATH = "./src/callbacks/img"
 
 class VisualizeLatents(Callback):
-    
     def __init__(
             self, 
-            # transform, 
-            # n_components,
-            # save_path,
-            # show,
-            # fig_kw,
-            # ax_kw,
-            # ax_title,
+            transform: Union[PCA, UMAP, TSNE, Isomap, Any], 
+            n_components: int,
+            save_path: str,
+            show: bool,
+            fig_kw: dict,
+            ax_kw: dict,
         ):
         super().__init__()
-        # self.transform = transform
-        # self.n_components = n_components
+        self.transform = transform
+        self.n_components = n_components
+
+        if save_path is None:
+            save_path = TEST_SAVE_PATH
+
+        save_path = Path(save_path)
+        if not save_path.exists():
+            save_path.mkdir(parents=True, exist_ok=True)
+        if not save_path.is_dir():
+            raise ValueError(f"Provided save_path {save_path} is not a directory")
+        self.save_path = save_path
+
+        self.show = show
+        self.fig_kw = fig_kw
+        self.ax_kw = ax_kw
 
         self.embeddings = []
         self.labels = []
@@ -32,36 +53,57 @@ class VisualizeLatents(Callback):
         # TODO: offload to async thread on CPU?
 
         embeddings = torch.vstack(self.embeddings).cpu().numpy()
-        print(embeddings.shape)  # DEBUG
 
         # aggregate labels across batches
         # for arbitrary number of labels
         aggregated_labels = {}
         for d in self.labels:
-            for label, arr in d.items():
-                if label not in aggregated_labels:
-                    aggregated_labels[label] = []
-                aggregated_labels[label].append(arr)
+            for name, arr in d.items():
+                if name not in aggregated_labels:
+                    aggregated_labels[name] = []
+                aggregated_labels[name].append(arr)
 
-        labels = {key: torch.hstack(value).cpu().numpy() for key, value in aggregated_labels.items()}
+        labels = {name: torch.hstack(arr).cpu().numpy() 
+                    for name, arr in aggregated_labels.items()}
 
-        for key, value in labels.items():  # DEBUG
-            print(f"{key}: {value.shape}")
+        transformed_embeddings = viz.transform(
+            embeddings,
+            transform=self.transform,
+            n_components=self.n_components,
+        )
 
-        # transformed_embeddings = viz.transform(
-        #     embeddings,
-        #     transform=self.transform,
-        #     n_components=self.n_components,
-        # )
+        try:
+            transform = self.transform.__class__.__name__
+        except AttributeError:
+            transform = str(self.transform)
+        epoch = trainer.current_epoch
+        
+        for name in labels.keys():
+            fig = viz.plot(
+                transformed_embeddings,
+                n_components=self.n_components,
+                labels=labels[name],
+                show=self.show,
+                save_path=self.save_path/f"{transform}_epoch={epoch}_label={name}.png",
+                fig_kw=self.fig_kw,
+                ax_kw=self.ax_kw,
+                ax_title=f"{name}",
+            )
 
-        # for d in range(labels.dim(0)):
-        # plot transformed embeddings
+            # TODO: log plot for each label
+            # wandb.log({name: fig}) # converts to Plotly
+            # wandb.log({name: wandb.Image(fig)}) # converts to static image
 
-        # pass embeddings and labels to plot function
-        # log plot as wandb artefact
-        # wandb = pl_module.logger.experiment
-        # wandb.log({"embeddings": wandb.Image(plot)})
-        # OR pl_module.log()
+            # see example from website
+            # trainer.logger.experiment.log(
+            #     {"samples": [wandb.Image(img, caption=caption) for (img, caption) in my_images]},
+            #     step=current_trainer_global_step,
+            # )
 
         self.embeddings.clear()
         self.labels.clear()
+
+    def on_train_end(self, trainer, pl_module):
+        # TODO: create GIF from saved images
+        # TODO: save final model checkpoint?
+        return
