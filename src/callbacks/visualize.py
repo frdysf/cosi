8779a215c89eadaf51839db4cf9ea22f7b4c35aa
Farida Yusuf <f.yusuf@qmul.ac.uk>
2025-07-03 12:49:04 +0000

    Handle multiprocess

--- src/callbacks/visualize.py
+++ src/callbacks/visualize.py
@@ -1,7 +1,9 @@
 from lightning.pytorch.callbacks import Callback
-import torch
-import wandb
 import viz
+
+from lightning.pytorch.utilities import rank_zero_only
+import torch
+import torch.distributed as dist
 
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE, Isomap
@@ -10,8 +12,11 @@ from umap.umap_ import UMAP
 from typing import Union, Any
 from pathlib import Path
 
+from lightning.pytorch.loggers import WandbLogger
+import wandb
 
-CMAP = {
+
+CMAP = {  # assign colormaps for preknown labels
     "pitch": "viridis",
     "modulation": "tab20",
     "instrument_family": "tab20",
@@ -42,28 +47,29 @@ class VisualizeLatents(Callback):
         self.fig_kw = fig_kw
         self.ax_kw = ax_kw
 
-    def on_validation_end(self, trainer, pl_module):
-        # TODO: async with other callbacks?
-        embeddings = pl_module.stacked_embeddings
-        labels = pl_module.stacked_labels
+    @rank_zero_only
+    def visualize(self, trainer, embeddings, labels):
+        embeddings = embeddings.cpu().numpy()
+        labels = {name: arr.cpu().numpy() for name, arr in labels.items()}
 
-        transformed_embeddings = viz.transform(
-            embeddings,
-            transform=self.transform,
-            n_components=self.n_components,
-        )
+        reduced_embeddings = viz.reduce_embeddings(
+                embeddings,
+                transform=self.transform,
+                n_components=self.n_components,
+            )
 
         try:
             transform = self.transform.__class__.__name__
         except AttributeError:
             transform = str(self.transform)
         epoch = trainer.current_epoch
+
         for name in labels.keys():
-            fig = viz.plot(
-                transformed_embeddings,
+            fig = viz.plot_reduced_embeddings(
+                reduced_embeddings,
                 n_components=self.n_components,
                 labels=labels[name],
-                cmap=CMAP[name],
+                cmap=CMAP.get(name, None),
                 show=self.show,
                 save_path=self.save_path/f"{transform}_epoch={epoch}_label={name}.png",
                 fig_kw=self.fig_kw,
@@ -72,9 +78,30 @@ class VisualizeLatents(Callback):
             )
 
             trainer.logger.experiment.log(
-                {f"latent/{name}": wandb.Image(fig),
-                 "epoch": epoch},
+                {f"latent/{name}": wandb.Image(fig)  # plotly begone
+                    if isinstance(trainer.logger, WandbLogger) else fig,
+                "epoch": epoch},
             )
+
+    def on_validation_end(self, trainer, pl_module):
+        local_embeddings = pl_module.stacked_embeddings
+        local_labels = pl_module.stacked_labels
+
+        # gather embeddings and labels from all processes
+        if dist.is_initialized() and dist.get_world_size() > 1:
+            world_size = dist.get_world_size()
+            gathered_embeddings = [torch.zeros_like(local_embeddings) for _ in range(world_size)]
+            gathered_labels = [None for _ in range(world_size)]
+
+            dist.gather(local_embeddings, gathered_embeddings)
+            dist.gather_object(local_labels, gathered_labels)
+
+            local_embeddings = torch.cat(gathered_embeddings, dim=0)
+            local_labels = {name: torch.cat([label[name] 
+                       for label in gathered_labels])
+                         for name in gathered_labels[0].keys()}
+
+        self.visualize(trainer, local_embeddings, local_labels)
 
     def on_train_end(self, trainer, pl_module):
         # TODO: create and organised saved images into
